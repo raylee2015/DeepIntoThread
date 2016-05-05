012aa18f4d923a439d01e940d029de1bf387bdac
R <R@R-PC>
2016-05-05 22:35:13 +0800

    从头认识多线程-2.12 synchronized ()代码块不单可以用this，也可以用其他对象

---
+++ src/com/ray/deepintothread/ch02/topic_13/ObjectLock.java
@@ -0,0 +1,84 @@
+package com.ray.deepintothread.ch02.topic_13;
+
+/**
+ * 
+ * @author RayLee
+ *
+ */
+public class ObjectLock {
+	public static void main(String[] args) throws InterruptedException {
+		MyService myService = new MyService();
+		ThreadOne threadOne = new ThreadOne(myService);
+		Thread thread = new Thread(threadOne);
+		thread.start();
+		ThreadTwo threadTwo = new ThreadTwo(myService);
+		Thread thread2 = new Thread(threadTwo);
+		thread2.start();
+	}
+}
+
+class ThreadOne implements Runnable {
+
+	private MyService myService;
+
+	public ThreadOne(MyService myService) {
+		this.myService = myService;
+	}
+
+	@Override
+	public void run() {
+		try {
+			myService.updateA();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class ThreadTwo implements Runnable {
+
+	private MyService myService;
+
+	public ThreadTwo(MyService myService) {
+		this.myService = myService;
+	}
+
+	@Override
+	public void run() {
+		try {
+			myService.updateB();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class MyService {
+
+	private Object object;
+
+	public MyService() {
+		object = new Object();
+	}
+
+	public void updateA() throws InterruptedException {
+		synchronized (object) {
+			long startTime = System.currentTimeMillis();
+			System.out.println("updateA startTime:" + startTime);
+			Thread.sleep(1000);
+			long endTime = System.currentTimeMillis();
+			System.out.println("updateA endTime:" + endTime);
+		}
+	}
+
+	public void updateB() throws InterruptedException {
+		synchronized (object) {
+			long startTime = System.currentTimeMillis();
+			System.out.println("updateB startTime:" + startTime);
+			Thread.sleep(1000);
+			long endTime = System.currentTimeMillis();
+			System.out.println("updateB endTime:" + endTime);
+		}
+	}
+
+}
