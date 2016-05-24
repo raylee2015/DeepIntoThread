10aac6aa0d9a11f229e36ee2800f7b01eaaa80de
raylee2015 <lilinghui-123@163.com>
2016-05-24 10:49:07 +0800

    从头认识多线程-2.25 synchronized同步方法在jvm是怎样执行的？ http://blog.csdn.net/raylee2007/article/details/51488104

---
+++ src/com/ray/deepintothread/ch02/topic_25/SynchMethod.java
@@ -0,0 +1,14 @@
+package com.ray.deepintothread.ch02.topic_25;
+
+/**
+ * 
+ * @author RayLee
+ *
+ */
+public class SynchMethod {
+	public synchronized void test1() {
+	}
+
+	public void test2() {
+	}
+}
